613cd69e78d3440bed695b3c24e2251d20766d2b
Dagmar Collela <dagmamirk@gmail.com>
2022-07-24 17:14:43 +0100

    To represent the image that appears in the highlight, it is filtering the list of movies that come from the original slug.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import tmdb from "./tmdb";
 import MovieRow from "./components/MovieRow";
+import FeatureMovie from "./components/FeatureMovie";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
   useEffect(() => {
     const loadAll = async () => {
       // Get all list
       let list = await tmdb.getHomeList();
       setMovieList(list);
+
+      // getting the feature
+      let originals = list.filter((i) => i.slug === "originals");
     };
     loadAll();
   }, []);
 
   return (
     <div className="page">
+      {featureData && <FeatureMovie item={featureData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeatureMovie.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./FeatureMovie.css";
+
+export default () => {
+  return <div>Feature Movie</div>;
+};
