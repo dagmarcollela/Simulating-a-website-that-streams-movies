3fa3a916530e67706e0a21a7286d454f79700f09
Dagmar Collela <dagmamirk@gmail.com>
2022-07-24 17:19:06 +0100

    Js function to get an aleatory number from the total of items in the array

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ export default () => {
 
       // getting the feature
       let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * originals[0].items.results.length - 1
+      );
+      let chosen = originals[0].items.results[randomChosen];
+
+      console.log(chosen);
     };
     loadAll();
   }, []);
