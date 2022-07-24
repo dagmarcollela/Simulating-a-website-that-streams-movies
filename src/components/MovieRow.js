85c15dc16f081b3e3c8740c829c725389faf64da
Dagmar Collela <dagmamirk@gmail.com>
2022-07-24 16:36:09 +0100

    Shows category titles and movie images

---
+++ src/components/MovieRow.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-anonymous-default-export */
+
+import React from "react";
+import "./MovieRow.css";
+
+export default ({ title, items }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="movieRow--arealist">
+        {items.results.length > 0 &&
+          items.results.map((item, key) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+              alt={item.original_title}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
